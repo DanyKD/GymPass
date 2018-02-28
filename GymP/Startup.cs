5c2dbb13be2fd4bbb5b3b61364f0c1d83a5df7ce
User <User@DESK-FG2GJ32>
2018-02-28 16:23:07 +0100

    Add project files.

---
+++ GymP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GymP.Startup))]
+namespace GymP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
